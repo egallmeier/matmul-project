7e75936a22e982f1351ba75c31acc4b7ffac1b65
egallmeier <eg542@cornell.edu>
2024-09-16 22:53:06 +0000

    changed loop order

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,14 +1,14 @@
 const char* dgemm_desc = "My awesome dgemm.";
-// this is a comment
+
 void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
     int i, j, k;
-    for (i = 0; i < M; ++i) {
-        for (j = 0; j < M; ++j) {
-            double cij = C[j*M+i];
-            for (k = 0; k < M; ++k)
-                cij += A[k*M+i] * B[j*M+k];
-            C[j*M+i] = cij;
+    for (j = 0; j < M; ++j) {
+        for (k = 0; k < M; ++k) {
+            double cjk = C[k*M+j];
+            for (i = 0; i < M; ++i)
+                cjk += A[i*M+j] * B[k*M+i];
+            C[k*M+j] = cjk;
         }
     }
 }
